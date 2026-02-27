2bd9e9eaaad97ef88f68d64b2b7dd9f105dd9907
Ferdous <ferdous1996h@gmail.com>
2026-02-28 00:43:52 +0600

    first commit

---
+++ data.js
@@ -0,0 +1,26 @@
+export const menuArray = [
+    {
+        name: "Pizza",
+        ingredients: ["pepperoni", "mushrom", "mozarella"],
+        id: 0,
+        price: 14,
+        emoji: "🍕",
+        img:'./IMG/pizza.png'
+    },
+    {
+        name: "Hamburger",
+        ingredients: ["beef", "cheese", "lettuce"],
+        price: 12,
+        emoji: "🍔",
+        id: 1,
+        img:'./IMG/burger.png'
+    },
+        {
+        name: "Beer",
+        ingredients: ["grain, hops, yeast, water"],
+        price: 19,
+        emoji: "🍺",
+        id: 2,
+        img:'./IMG/beer.png'
+    }
+]
