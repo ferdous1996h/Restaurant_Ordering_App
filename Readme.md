07f83afe6b552ca9fde66f867ba8c0dd89233302
Ferdous <ferdous1996h@gmail.com>
2026-03-07 16:03:40 +0600

    Add Darkmode and remove item feature

--- Readme.md
+++ Readme.md
@@ -1,7 +1,21 @@
-## Resturant_Ordering_App
+# Resturant_Ordering_App
 
-### Features need to be added
-- remove item from buying list 
-- discount functionality 
-- 5 star review section after puchase
-- dynamic buying list
+## Feature
+
+- Can add item from menu to cart
+- can remove item from cart
+- Dynamic cost calculation
+- Popup modal for order confirmation
+- Thank U screen
+- Dark mode feature with LocalStorage
+
+## Skill acquired
+
+- Use event delegation for instant event response
+- Object mutation and render data according to that
+- avoid multiple eventloop
+- Implement darkmode in localstorage to keep darkmode status in browser
+
+## TechStack Used
+
+![TechStack](https://skillicons.dev/icons?i=html,css,js,react,bun,vite)
--- script.js
+++ script.js
@@ -1,71 +1,133 @@
-"use strict";
-import { menuArray } from "./data.js";
-const grabInfoSec = document.querySelector(".details_info");
-const itemBOX = document.querySelector(".item__box");
-const expeseResult = document.querySelector(".net__expense");
-const submitPayment = document.querySelector(".order__submit--btn");
-const popupMenu = document.querySelector(".popup_section");
-const orderSec = document.querySelector(".order__section");
-const tnxSec = document.querySelector(".thank_you--sec");
-const infoSend = document.querySelector(".pay__submit");
-const form = document.getElementById("myForm");
-let sum = [];
+'use strict';
+import { menuArray } from './data.js';
+import { nanoid } from 'nanoid';
+const grabInfoSec = document.querySelector('.details_info');
+const itemBOX = document.querySelector('.item__box');
+const expeseResult = document.querySelector('.net__expense');
+const submitPayment = document.querySelector('.order__submit--btn');
+const popupMenu = document.querySelector('.popup_section');
+const orderSec = document.querySelector('.order__section');
+const tnxSec = document.querySelector('.thank_you--sec');
+const infoSend = document.querySelector('.pay__submit');
+const form = document.getElementById('myForm');
 let purchaseInfo = [];
-menuArray.forEach((item) => {
+let order = false;
+menuArray.forEach(item => {
   console.log(item);
-  const card = document.createElement("div");
-  card.className = "app__card";
+  const card = document.createElement('div');
+  card.className = 'app__card';
   card.innerHTML = `
         <img src=${item.img} alt="" class="app__card-icon">
-        <div class="card_info">
+        <section class="card_info">
           <h2>${item.name}</h2>
-          <p style="color: #8B8B8B;">${item.ingredients}</p>
+          <p>${item.ingredients}</p>
           <h4>$${item.price}</h4>
-        </div>
+        </section>
         <button class="card-btn">
           <i class="fa-solid fa-plus"></i>
         </button>
   `;
-  const punchItem = card.querySelector(".card-btn");
-  punchItem.addEventListener("click", (e) => {
-    purchaseInfo.push({
-      name: item.name,
-      price: item.price,
-    });
-    tnxSec.style.display = "none";
-    orderSec.style.display = "block";
-    const data = document.createElement("div");
-    data.className = "order_item";
-    data.innerHTML = `
-         <!-- <p>${item.name}<button class="remove_item">❌</button></p>-->
-          <p>${item.name}</p>
-          <p>$${item.price}</p>
-    `;
-    itemBOX.appendChild(data);
-    sum.push(item.price);
-    expeseResult.textContent = sum.reduce((a, b) => a + b, 0);
 
-    // data.querySelector(".remove_item").addEventListener("click", () => {
-    //   console.log("hi");
-    //   console.log(purchaseInfo);
-    // });
-  });
   grabInfoSec.appendChild(card);
 });
-submitPayment.addEventListener("click", () => {
-  popupMenu.style.display = "block";
+
+grabInfoSec.addEventListener('click', e => {
+  if (
+    e.target.classList.contains('card-btn') ||
+    e.target.classList.contains('fa-plus')
+  ) {
+    order = false;
+    renderTnx();
+    const uniqueId = nanoid();
+    const parentDiv = e.target.closest('.app__card');
+    const infoDiv = parentDiv.querySelector('.card_info');
+    const itemName = infoDiv.querySelector('h2').textContent;
+    const itemPrice = infoDiv.querySelector('h4').textContent.slice(1);
+    console.log(itemName, itemPrice);
+    purchaseInfo.push({
+      id: uniqueId,
+      name: itemName,
+      price: Number(itemPrice),
+    });
+    renderPayment();
+  }
 });
 
-form.addEventListener("submit", (e) => {
+function renderPayment() {
+  itemBOX.innerHTML = '';
+  orderSec.style.display = 'block';
+  purchaseInfo.map(ele => {
+    itemBOX.innerHTML += `
+    <div class='order_item'>
+    <p>${ele.name}<button class="remove_item" data-num=${ele.id}>❌</button></p>
+    <p>$${ele.price}</p>
+    </div>
+    `;
+    expeseResult.textContent = purchaseInfo
+      .map(ele => ele.price)
+      .reduce((a, c) => a + c, 0);
+  });
+}
+
+itemBOX.addEventListener('click', e => {
+  if (e.target.classList.contains('remove_item')) {
+    const targetEle = e.target.dataset.num;
+    purchaseInfo = purchaseInfo.filter(ele => ele.id !== targetEle);
+    console.log(purchaseInfo);
+    renderPayment();
+    expeseResult.textContent = purchaseInfo
+      .map(ele => ele.price)
+      .reduce((a, c) => a + c, 0);
+  }
+});
+
+submitPayment.addEventListener('click', () => {
+  popupMenu.style.display = 'block';
+});
+
+form.addEventListener('submit', e => {
   e.preventDefault();
-  const id = document.getElementById("name");
+  const id = document.getElementById('name');
   console.log(id.value);
-  document.querySelector(".buyer").textContent = id.value;
-  popupMenu.style.display = "none";
-  tnxSec.style.display = "block";
-  orderSec.style.display = "none";
-  sum = [];
-  itemBOX.innerHTML = "";
+  document.querySelector('.buyer').textContent = id.value;
+  popupMenu.style.display = 'none';
+  order = true;
+  renderTnx();
+  orderSec.style.display = 'none';
+  itemBOX.innerHTML = '';
+  purchaseInfo=[];
   form.reset();
 });
-// gu
+
+function renderTnx() {
+  if (order) tnxSec.style.display = 'block';
+  else tnxSec.style.display = 'none';
+}
+
+
+// dark mode
+const displayData = localStorage.getItem('displayMode');
+if(displayData==='light'){
+  document.querySelector('.app').classList.remove('dark_Mode');
+}else{
+  document.querySelector('.app').classList.add('dark_Mode');
+}
+
+
+
+document.querySelector('.toggle_Mode').addEventListener('click',()=>{
+  const data=localStorage.getItem('displayMode')
+
+  if(data==='light'){
+    document.querySelector('.app').classList.add('dark_Mode');
+    localStorage.setItem('displayMode', 'dark');
+  }
+  if(data==='dark'){
+    document.querySelector('.app').classList.remove('dark_Mode');
+    localStorage.setItem('displayMode', 'light');
+  }
+  if (!data) {
+    document.querySelector('.app').classList.remove('dark_Mode');
+    localStorage.setItem('displayMode', 'light');
+  }
+});
